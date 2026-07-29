62cb84a68a8710c5ce1c10759fb85ad44e3c9cf3
bonnefigure <francoisfave7@gmail.com>
2026-07-29 23:47:23 +0200

    Update next-project.js

--- next-project.js
+++ next-project.js
@@ -25,8 +25,11 @@
 
     var nextSlug = order[(idx + 1) % order.length];
 
-    var btns = document.querySelectorAll('.btn-next');
     btns.forEach(function (btn) {
-        btn.setAttribute('href', '../' + nextSlug + '/');
+    btn.setAttribute('href', '../' + nextSlug + '/');
+
+    btn.addEventListener('click', function () {
+        sessionStorage.removeItem('cameFromIndex');
     });
+});
 })();
